55073fafd21d454d311faefcb66fb4d8baab651d
emelyne1234 <e.uwimbabazai@alustudent.com>
2023-02-10 03:35:19 -0800

    ops

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" about minimum operations """
+
+def minOperations(n):
+    """determines the lowest number of operations"""
+    operations = 0
+    x = 2
+    while n > 1:
+        while n % x == 0:
+            operations += 1
+            n /= x
+        x += 1
+    return operations
