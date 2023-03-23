c8e7c37f6bca546eceff353ee5e3551063101375
emelyne1234 <e.uwimbabazi@alustudent.com>
2023-03-23 14:55:10 +0000

    pascal triangle

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""prints the triangle"""
+
+
+def pascal_triangle(n):
+    """prints the list representing pascal's principle"""
+    if n <= 0:
+        return = []
+    triangle = [[1], [1, 1]]
+    for x in range(2, n):
+        row_previous = [0] + triangle[x - 1] + [0]
+        row = []
+        for y in range(0, len(row.previous) - 1):
+            row.append(row_previous[y] + row_previous[y + 1])
+            triangle.append(row)
+    return triangle[:n]
