fd1353b810733afb9fc561e02c913b3fcdd81424
emelyne1234 <e.uwimbabazi@alustudent.com>
2023-03-23 15:05:15 +0000

    pascal triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -9,7 +9,7 @@ def pascal_triangle(n):
     for x in range(2, n):
         row_previous = [0] + triangle[x - 1] + [0]
         row = []
-        for y in range(0, len(row.previous) - 1):
+        for y in range(0, len(row_previous) - 1):
             row.append(row_previous[y] + row_previous[y + 1])
             triangle.append(row)
     return triangle[:n]
